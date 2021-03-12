a2f3d0afe4a80702f48165b847f94ef2e3a768ed
pedrojfs17 <up201806227@fe.up.pt>
2021-03-12 10:41:31 +0000

    Removed unnecessary code

--- html/js/carousel.js
+++ html/js/carousel.js
@@ -21,21 +21,3 @@ carouselItems.forEach(item => {
         item.appendChild(next.firstElementChild.cloneNode(true))
     }
 })
-
-// $('.carousel .carousel-item').each(function(){
-//     var minPerSlide = 3;
-//     var next = $(this).next();
-//     if (!next.length) {
-//     next = $(this).siblings(':first');
-//     }
-//     next.children(':first-child').clone().appendTo($(this));
-    
-//     for (var i=0;i<minPerSlide;i++) {
-//         next=next.next();
-//         if (!next.length) {
-//             next = $(this).siblings(':first');
-//         }
-        
-//         next.children(':first-child').clone().appendTo($(this));
-//     }
-// });

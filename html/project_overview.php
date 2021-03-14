bd1f4c5ef3c2543d95944a2bf2c6619a1c56c69f
pedrojfs17 <up201806227@fe.up.pt>
2021-03-14 15:28:13 +0000

    Fixed header in project

--- html/project_overview.php
+++ html/project_overview.php
@@ -8,12 +8,12 @@ include_once('templates/tpl_common.php');
 
 <header class="page-header header container-md">
     <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="#">Sourdough Baking</a>
+        <a class="navbar-brand text-dark" href="#">Sourdough Baking</a>
         <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#main-navigation-overview" aria-controls="main-navigation-overview" aria-expanded="false" aria-label="Toggle navigation">
             <i class="bi bi-caret-down project-nav-toggler"></i>
         </button>
         <div class="collapse navbar-collapse" id="main-navigation-overview">
-            <ul class="navbar-nav d-lg-flex w-100 px-5 align-items-lg-center">
+            <ul class="navbar-nav d-lg-flex w-100 px-5 align-items-lg-end">
                 <li class="nav-item">
                     <a class="nav-link active" href="project_overview.php">Overview</a>
                 </li>
@@ -30,7 +30,7 @@ include_once('templates/tpl_common.php');
                     <a class="nav-link" href="project_settings.php"><span class="d-lg-none">Preferences</span><i class="bi bi-gear me-2 d-none d-lg-inline-block"></i></a>
                 </li>
                 <li class="nav-item ms-lg-auto">
-                    <a class="nav-link d-flex align-items-center" href="#"><span class="mx-lg-2">Add Task</span> <i class="bi bi-plus-circle fs-4 d-none d-lg-inline-block"></i></a>
+                    <a class="nav-link d-flex align-items-center" style="margin-right: 0.5em !important;" href="#"><span class="mx-lg-2">Add Task</span> <i class="bi bi-plus-circle fs-4 d-none d-lg-inline-block"></i></a>
                 </li>
             </ul>
         </div>

ca849c7342f50d55d67fac2eafe0a050a8cc3d07
up201806451 <up201806451@fe.up.pt>
2021-05-17 01:14:21 +0100

    Found bug
    Modal is being passed by ajax, messing up both tags and delete scripts

--- public/js/deleteTask.js
+++ public/js/deleteTask.js
@@ -1,11 +1,15 @@
-const taskDelete = document.querySelector('#taskDelete')
-console.log(taskDelete)
-taskDelete.addEventListener('click', function (e) {
-  sendDeleteAjaxRequest(taskDelete.dataset.href)
-})
+const modal = document.querySelector('.modal-container')
+console.log(modal)
+
+if(modal.children.length>0){
+  const taskDelete = document.getElementById('deleteTask')
+  taskDelete.addEventListener('click', function (e) {
+    sendDeleteAjaxRequest(taskDelete.dataset.href)
+  })
+}
+
 
 function sendDeleteAjaxRequest(route) {
-
   let xhr = new XMLHttpRequest();
   xhr.open("DELETE", route);
   xhr.send();

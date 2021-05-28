68552666e93e9ae0f0a62c70494897ec14e9ae25
Ines Oliveira e Silva <inesoliveiraesilva@gmail.com>
2021-05-28 11:31:54 +0100

    Admin stats page responsive

--- resources/views/pages/adminStatistics.blade.php
+++ resources/views/pages/adminStatistics.blade.php
@@ -32,32 +32,32 @@
   @csrf
 
   <div class="container-md">
-    <div class="d-flex justify-content-between mt-3">
-      <div class="col-6 d-flex flex-column">
+    <div class="d-flex flex-column flex-md-row justify-content-between mt-3">
+      <div class="col-md-6 d-flex flex-column">
         <h1 class="mb-3">User Stats</h1>
         <div class="d-flex flex-column justify-content-center flex-grow-1">
-          <div class=" d-flex justify-content-around me-4 shadow p-3" style="border-radius: 15px">
-            <div class="text-center">
+          <div class=" d-flex flex-wrap justify-content-around m-4 shadow p-3" style="border-radius: 15px">
+            <div class="text-center mx-2">
               <h1 class="display-6">{{$total_users}}</h1>
               <h5>Total Users</h5>
             </div>
-            <div class="text-center">
+            <div class="text-center mx-2">
               <h1 class="display-6">{{$women}}%</h1>
               <h5>Women</h5>
             </div>
-            <div class="text-center">
+            <div class="text-center mx-2">
               <h1 class="display-6">{{$men}}%</h1>
               <h5>Men</h5>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-6 px-2 align-items-center">
+      <div class="col-md-6 px-2 align-items-center">
         <h1 class="mb-3">Top Countries</h1>
         @foreach($countries as $country => $n)
         <div class="d-flex">
-          <h5 class="col-3">{{$country}}</h5>
-          <div class="progress col-9">
+          <h5 class="col-4 text-truncate me-2">{{$country}}</h5>
+          <div class="progress col-8">
             <div class="progress-bar" role="progressbar" style="width: {{$n}}%;background-color: #00AFB9"
                  aria-valuenow="{{$n}}" aria-valuemin="0" aria-valuemax="100">{{$n}}%
             </div>
@@ -69,7 +69,7 @@
 
   </div>
   <div class="container-md">
-    <div class="d-flex justify-content-between mt-3 stats-dashboard ">
+    <div class="d-flex justify-content-between mt-3">
       <div class="d-flex flex-column flex-grow-1">
 
         <div class="row align-items-center">
@@ -77,20 +77,20 @@
             <h1 class="mb-3">Project Stats</h1>
           </div>
         </div>
-        <div class="row align-items-center">
-          <div class="col-lg-3 order-lg-3">
+        <div class="d-flex flex-wrap align-content-stretch justify-content-between">
+          <div class="" style="width: 200px">
             <h1 class="display-6">{{$total_projects}}</h1>
             <h5>Total Projects</h5>
           </div>
-          <div class="col-lg-3 order-lg-3">
+          <div class="" style="width: 200px">
             <h1 class="display-6">{{$completed_projects}}</h1>
             <h5>Projects completed</h5>
           </div>
-          <div class="col-lg-3 order-lg-3">
+          <div class="" style="width: 200px">
             <h1 class="display-6">{{$avg_projects}}</h1>
             <h5>Avg Projects/User</h5>
           </div>
-          <div class="col-lg-3 order-lg-3">
+          <div class="" style="width: 200px">
             <h1 class="display-6">{{$avg_progress}}%</h1>
             <h5>Avg Project Progress</h5>
           </div>

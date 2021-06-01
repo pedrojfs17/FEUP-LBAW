88905bc8c59a377e956180933e99e7eef3b0e3dc
antbz <antonio.bezerra@fe.up.pt>
2021-06-01 16:50:10 +0100

    Made client cards clickable

--- resources/views/partials/memberCard.blade.php
+++ resources/views/partials/memberCard.blade.php
@@ -2,10 +2,11 @@
   <div class="card-body">
     <img class="rounded-circle d-inline-block mx-2" src="{{ url($member->avatar) }}" width="40px" height="40px"
          alt="avatar">
-    <h5 class="card-title d-inline-block">{{ $member->account->username }}</h5>
-    <h6 class="d-inline-block text-muted">{{ $member->account->email }}</h6>
+    <h5 class="card-title d-inline-block">{{ $member->fullname }}</h5>
+    <h6 class="d-inline-block text-muted">{{ $member->account->username }}</h6>
+    <a class='text-decoration-none stretched-link' href="/profile/{{$member->account->username}}"></a>
     @if (Auth::user()->is_admin)
-      <button class="btn btn-danger float-end remove-button" data-href="/profile/{{ $member->account->username }}" type="button">Remove</button>
+      <button class="btn btn-danger float-end remove-button" data-href="/profile/{{ $member->account->username }}" type="button" style="z-index: 1; position:relative;">Remove</button>
     @endunless
   </div>
 </div>

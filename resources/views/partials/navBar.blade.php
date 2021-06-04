c04b69b3adc9973d6ce9d90de82ca7ad2b5140f0
antbz <antonio.bezerra@fe.up.pt>
2021-06-04 16:54:05 +0100

    bugfix

--- resources/views/partials/navBar.blade.php
+++ resources/views/partials/navBar.blade.php
@@ -10,11 +10,13 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                @if (!Auth::user()->is_admin)
                 <li class="nav-item">
                     <a class="nav-link fs-5" href="{{route('search')}}" id="toggle-search"><i class="bi bi-search"></i>
                         <p class="d-inline-block d-sm-none ps-2 mb-0">Search</p>
                     </a>
                 </li>
+                @endif
                 {{--<li class="nav-item">
                     <a class="nav-link fs-5" href="#" --}}{{--data-bs-toggle="modal" data-bs-target="#notificationsModal"--}}{{-->
                         <i class="bi bi-bell"></i>
@@ -27,7 +29,7 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link" href="" id="profileDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         @if (Auth::user()->is_admin)
-                        Admin
+                        {{ Auth::user()->email }}
                         @else
                         <img class="rounded-circle me-2" src="{{ url(Auth::user()->client->avatar) }}" width="30" height="30" alt="avatar" id='navBarAvatar'>
                         {{ Auth::user()->username }}
